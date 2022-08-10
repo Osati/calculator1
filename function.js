f1d28f36711bb85eb83cc9b9eb9a276ad92b9770
Aniscs452 <106110762+Osati@users.noreply.github.com>
2022-08-10 23:53:28 +0600

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+
+
+function store(value){
+  form.panel.value = form.panel.value + value;
+
+}
+
+function ce(){
+  form.panel.value = "";
+}
+function back(){
+  var value = form.panel.value;
+  //alert(value.length);
+  form.panel.value = value.substr(0,value.length-1);
+}
+
+function calculator(cal){
+  form.panel.value = eval(cal);
+}
